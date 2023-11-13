3adc94255ee2c487e3162e7956400cffb356bb09
Owen <owen.rotich@strathmore.edu>
2023-11-13 16:59:47 +0300

    reverted to feed-forward ANNs

--- model/utils.py
+++ model/utils.py
@@ -19,37 +19,66 @@ class Preprocessor:
     def fit(self, df):
         urls = df['url']
         tlds = urls.apply(lambda x: tldextract.extract(x).suffix)
+
+        # Tokenizing the URLs
         self.tokenizer.fit_on_texts(urls)
         sequences = self.tokenizer.texts_to_sequences(urls)
         self.sequences_len = max([len(seq) for seq in sequences]) # measure the maximum number of pad sequences
         sequences = pad_sequences(sequences, maxlen=self.sequences_len) # set the maximum length
         print(f'length of sequences: {len(sequences[0])}')
+
+        # Extract features
         features = self.extract_features(df)
+
+        # one-hot encode TLDs
         tlds = self.one_hot_encoder.fit_transform(np.array(tlds).reshape(-1, 1)).toarray()
         all_data = np.concatenate([sequences, features, tlds], axis=1)
+        
+        # Scale the data
         self.scaler.fit(all_data)
+        return
 
     def transform(self, df):
         urls = df['url']
         tlds = urls.apply(lambda x: tldextract.extract(x).suffix)
+
+        # Tokenizing the URLs
         sequences = self.tokenizer.texts_to_sequences(urls)
         sequences = pad_sequences(sequences, maxlen=self.sequences_len) # set the maximum length
+        
+        # Extract features
         features = self.extract_features(df)
+
+        # one-hot encode TLDs
         tlds = self.one_hot_encoder.transform(np.array(tlds).reshape(-1, 1)).toarray()
         all_data = np.concatenate([sequences, features, tlds], axis=1)
+
+        # Scale the data
         return self.scaler.transform(all_data)
 
     def fit_transform(self, df):
         self.fit(df)
         return self.transform(df)
         
+
     def extract_features(self, df):
-      df['url_length'] = df['url'].apply(len)
-      df['num_digits'] = df['url'].apply(lambda x: sum(c.isdigit() for c in x))
-      df['num_letters'] = df['url'].apply(lambda x: sum(c.isalpha() for c in x))
-      df['num_special_chars'] = df['url'].apply(lambda x: len(re.findall('[^a-zA-Z0-9]', x)))
-      
-      return np.array(df.drop(columns=['url', 'label']))
+        df['url_length'] = df['url'].apply(len)
+        df['num_digits'] = df['url'].apply(lambda x: sum(c.isdigit() for c in x))
+        df['num_letters'] = df['url'].apply(lambda x: sum(c.isalpha() for c in x))
+        df['num_special_chars'] = df['url'].apply(lambda x: len(re.findall('[^a-zA-Z0-9]', x)))
+        df['num_dots'] = df['url'].apply(lambda x: x.count('.'))
+        df['num_slashes'] = df['url'].apply(lambda x: x.count('/'))
+        df['num_hyphens'] = df['url'].apply(lambda x: x.count('-'))
+
+        # Additional features
+        df['num_subdomains'] = df['url'].apply(lambda x: x.count('.'))
+        df['has_https'] = df['url'].apply(lambda x: 1 if x.startswith('https://') else 0)
+        df['tld_length'] = df['url'].apply(lambda x: len(tldextract.extract(x).suffix) if '.' in x else 0)
+        df['has_ip'] = df['url'].apply(lambda x: 1 if re.match(r"\b(?:\d{1,3}\.){3}\d{1,3}\b", x) else 0)
+        df['has_redirects'] = df['url'].apply(lambda x: 1 if '//' in x else 0)
+
+        return np.array(df.drop(columns=['url', 'label']))
+
 
 def is_phishing_url(url):
     # Load the preprocessor and the model
